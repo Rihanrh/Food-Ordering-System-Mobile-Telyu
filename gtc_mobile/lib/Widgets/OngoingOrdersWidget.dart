1db6cc3fdb249d2c35ef4a58b484a42966129355
jaatsiyah <jaatsiyah@student.telkomuniversity.ac.id>
2024-03-12 22:41:50 +0700

    Antrian Kantin UI

---
+++ gtc_mobile/lib/Widgets/OngoingOrdersWidget.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class OngoingOrdersWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.only(left: 10, right: 10, bottom: 5, top: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 12,top: 10),
+                child: Text(
+                  "Pesanan Sedang Berlangsung",
+                  style: GoogleFonts.poppins(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                    color: Color.fromRGBO(211, 36, 43, 1),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              for (int i = 1; i <= 2; i++)
+                Container(
+                  margin: EdgeInsets.all(25),
+                  padding: EdgeInsets.all(5),
+                  height: 140,
+                  width: 330,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 1,
+                        blurRadius: 6,
+                      ),
+                    ],
+                  ),
+                  child: Row(
+                    children: [
+                      // Text widget for the text
+                      Center(
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 60), // Adjust spacing as needed
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            // crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Saat ini kamu belum",
+                                style: GoogleFonts.poppins(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w600,
+                                  color: Color.fromRGBO(202, 37, 37, 1),
+                                ),
+                              ),
+                              Text(
+                                "memiliki pesanan :(",
+                                style: GoogleFonts.poppins(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w600,
+                                  color: Color.fromRGBO(202, 37, 37, 1),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
---
+++ gtc_mobile/lib/Widgets/OrdersQueueWidget.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class OrdersQueueWIdget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.only(left: 10, right: 10, bottom: 5, top: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 12, top: 10),
+                child: Text(
+                  "Antrian Kantin",
+                  style: GoogleFonts.poppins(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                    color: Color.fromRGBO(211, 36, 43, 1),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              for (int i = 1; i <= 4; i++)
+                Container(
+                  margin: EdgeInsets.all(25),
+                  padding: EdgeInsets.all(5),
+                  height: 100,
+                  width: 200,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 1,
+                        blurRadius: 6,
+                      ),
+                    ],
+                  ),
+                  child: Row(
+                    children: [
+                      // Text widget for the text
+                      Center(
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 10),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Kantin Rasya",
+                                style: GoogleFonts.poppins(
+                                  fontSize: 13,
+                                  fontWeight: FontWeight.w600,
+                                  color: Color.fromRGBO(126, 0, 0, 1),
+                                ),
+                              ),
+                              SizedBox(
+                                  height:
+                                      4), // Adjust spacing between lines as needed
+                              Text(
+                                "No Antrian : R122,R123",
+                                style: GoogleFonts.poppins(
+                                  fontSize: 13,
+                                  fontWeight: FontWeight.w700,
+                                  color: Color.fromRGBO(113, 0, 0, 0.5),
+                                ),
+                              ),
+                              SizedBox(
+                                  height:
+                                      4), // Adjust spacing between lines as needed
+                              Text(
+                                "13 Orders To Go",
+                                style: GoogleFonts.poppins(
+                                  fontSize: 13,
+                                  fontWeight: FontWeight.w700,
+                                  color: Color.fromRGBO(113, 0, 0, 0.5),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+            ],
+          ),
+        ),
+        
+      ],
+    );
+  }
+}
