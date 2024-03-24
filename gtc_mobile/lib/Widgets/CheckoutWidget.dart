26e3d6316e81cf0f8a349283ee25172018d74725
jaatsiyah <jaatsiyah@student.telkomuniversity.ac.id>
2024-03-24 21:07:09 +0700

    Metode Pembayran blm fix

--- gtc_mobile/lib/Widgets/CheckoutWidget.dart
+++ gtc_mobile/lib/Widgets/CheckoutWidget.dart
@@ -4,13 +4,17 @@ import 'package:google_fonts/google_fonts.dart';
 
 class CheckoutModal {
   static final _controller = ValueNotifier<bool>(false);
+  static String _paymentMethod = 'Tunai'; // Default payment method
 
   static void show(BuildContext context) {
-    showModalBottomSheet(
+    showModalBottomSheet<dynamic>(
+      isScrollControlled: true,
       context: context,
       builder: (BuildContext context) {
-        return Container(
-          padding: EdgeInsets.all(20),
+        return Wrap(
+          children: <Widget>[
+            Container(
+          padding: EdgeInsets.only(left: 0, right: 0, top: 10),
           child: Column(
             mainAxisSize: MainAxisSize.min,
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -29,75 +33,82 @@ class CheckoutModal {
                 title: Text(
                   'Nomor Meja',
                   style: GoogleFonts.poppins(
-                    fontSize: 15,
-                    fontWeight: FontWeight.w600,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
                     color: Color.fromRGBO(126, 0, 0, 1),
                   ),
                 ),
-                onTap: () {/* Add item 2 to cart */},
+                onTap: () {},
               ),
-              Container(
-                width: 335,
-                margin: EdgeInsets.symmetric(vertical: 10),
-                child: OutlinedButton(
-                  onPressed: () {},
-                  style: ButtonStyle(
-                    side: MaterialStateProperty.all(BorderSide(
-                      color: Color.fromRGBO(126, 0, 0, 1),
-                      width: 2,
-                    )),
-                    shape: MaterialStateProperty.all(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30.0),
+              Center(
+                child: Container(
+                  width: 335,
+                  margin: EdgeInsets.all(10),
+                  child: OutlinedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      side: MaterialStateProperty.all(BorderSide(
+                        color: Color.fromRGBO(126, 0, 0, 1),
+                        width: 2,
+                      )),
+                      shape: MaterialStateProperty.all(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
                       ),
                     ),
-                  ),
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(vertical: 10),
-                    child: Text(
-                      'Pilih Nomor Meja',
-                      style: GoogleFonts.poppins(
-                        color: Color.fromRGBO(126, 0, 0, 1),
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(vertical: 10),
+                      child: Text(
+                        'Pilih Nomor Meja',
+                        style: GoogleFonts.poppins(
+                          color: Color.fromRGBO(126, 0, 0, 1),
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
                   ),
                 ),
               ),
+              
               SizedBox(height: 10),
               Divider(
                 color: Colors.grey,
                 height: 1,
               ),
-              SizedBox(height: 20),
+              SizedBox(height: 10),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Nasi Goreng',
-                        style: GoogleFonts.poppins(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          color: Color.fromRGBO(51, 51, 51, 1),
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Nasi Goreng',
+                          style: GoogleFonts.poppins(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromRGBO(51, 51, 51, 1),
+                          ),
                         ),
-                      ),
-                      Text(
-                        'Rp. 25.000',
-                        style: GoogleFonts.poppins(
-                          color: Color.fromRGBO(183, 73, 73, 1),
-                          fontWeight: FontWeight.w600,
+                        Text(
+                          'Rp. 25.000',
+                          style: GoogleFonts.poppins(
+                            color: Color.fromRGBO(183, 73, 73, 1),
+                            fontWeight: FontWeight.w600,
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                   Spacer(),
                   Container(
                     width: 60,
                     height: 60,
+                    margin: EdgeInsets.only(right: 10),
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(10),
                       image: DecorationImage(
@@ -108,38 +119,41 @@ class CheckoutModal {
                   ),
                 ],
               ),
-              SizedBox(height: 20),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Expanded(
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: Text(
-                        'Catatan',
-                        style: GoogleFonts.poppins(
-                          color: Color.fromRGBO(177, 27, 27, 1),
-                          fontWeight: FontWeight.bold,
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Catatan',
+                          style: GoogleFonts.poppins(
+                            color: Color.fromRGBO(177, 27, 27, 1),
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                      style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all(Colors.white),
-                        shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                          side:
-                              BorderSide(color: Color.fromRGBO(177, 27, 27, 1)),
-                        )),
+                        style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.white),
+                          shape: MaterialStateProperty.all(
+                              RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                            side: BorderSide(
+                                color: Color.fromRGBO(177, 27, 27, 1)),
+                          )),
+                        ),
                       ),
                     ),
                   ),
-                  SizedBox(width: 5),
                   AdvancedSwitch(
                     controller: _controller,
                     enabled: true,
                     height: 35,
                     width: 150,
-                    borderRadius: const BorderRadius.all(Radius.circular(120)),
+                    borderRadius:
+                        const BorderRadius.all(Radius.circular(120)),
                     inactiveColor: Colors.grey,
                     activeColor: Color.fromRGBO(202, 37, 37, 1),
                     inactiveChild: Text(
@@ -159,7 +173,6 @@ class CheckoutModal {
                       ),
                     ),
                   ),
-                  SizedBox(width: 1),
                   IconButton(
                     icon: Icon(
                       Icons.remove_circle_outline,
@@ -181,8 +194,46 @@ class CheckoutModal {
                   ),
                 ],
               ),
+              SizedBox(height: 10),
+              Divider(
+                color: Colors.grey,
+                height: 1,
+              ),
+              SizedBox(height: 10),
+              ListTile(
+                title: Text(
+                  'Metode Pembayaran',
+                  style: GoogleFonts.poppins(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromRGBO(126, 0, 0, 1),
+                  ),
+                ),
+              ),
+              RadioListTile(
+                title: Text('Tunai'),
+                value: 'Tunai',
+                groupValue: _paymentMethod,
+                onChanged: (value) {
+                  setState((){
+                    _paymentMethod = value!;
+                  });
+                },
+              ),
+              RadioListTile(
+                title: Text('QRIS'),
+                value: 'QRIS',
+                groupValue: _paymentMethod,
+                onChanged: (value) {
+                  setState((){
+                    _paymentMethod = value!;
+                  });
+                },
+              ),
             ],
           ),
+        )
+          ],
         );
       },
     );
