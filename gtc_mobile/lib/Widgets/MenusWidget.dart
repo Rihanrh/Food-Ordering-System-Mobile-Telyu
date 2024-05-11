5d69d944fe0ef581713c8129e8a70ce87fda14c6
Rihanrh <rihanh@student.telkomuniversity.ac.id>
2024-05-12 02:12:51 +0700

    Remove some comments from MenusWidget

--- gtc_mobile/lib/Widgets/MenusWidget.dart
+++ gtc_mobile/lib/Widgets/MenusWidget.dart
@@ -25,48 +25,6 @@ class _MenusWidgetState extends State<MenusWidget> {
     loadTenantList();
   }
 
-  // void _incrementQuantity(String menuName) {
-  //   setState(() {
-  //     _menuQuantities[menuName] = (_menuQuantities[menuName] ?? 0) + 1;
-  //   });
-  // }
-
-  // void _decrementQuantity(String menuName) {
-  //   setState(() {
-  //     if (_menuQuantities[menuName] != null && _menuQuantities[menuName]! > 0) {
-  //       _menuQuantities[menuName] = (_menuQuantities[menuName]! - 1);
-  //     }
-  //   });
-  // }
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Column(
-  //     children: [
-  //       Padding(
-  //         padding: EdgeInsets.only(left: 10, right: 10, bottom: 10),
-  //         child: Row(
-  //           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-  //           children: [
-  //             Padding(
-  //               padding: EdgeInsets.only(left: 12, top: 10),
-  //               child: Text(
-  //                 "Daftar Menu",
-  //                 style: GoogleFonts.poppins(
-  //                   fontSize: 22,
-  //                   fontWeight: FontWeight.bold,
-  //                   color: Color.fromRGBO(211, 36, 43, 1),
-  //                 ),
-  //               ),
-  //             )
-  //           ],
-  //         ),
-  //       ),
-  //       for (var menuName in _menuQuantities.keys) _buildMenu(menuName),
-  //     ],
-  //   );
-  // }
-
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -95,7 +53,7 @@ class _MenusWidgetState extends State<MenusWidget> {
           builder: (context, snapshot) {
             if (snapshot.hasData) {
               return Container(
-                height: 700.0,
+                height: 1000.0,
                 child: ListView.builder(
                   itemCount: snapshot.data!.length,
                   scrollDirection: Axis.vertical,
@@ -134,122 +92,6 @@ class _MenusWidgetState extends State<MenusWidget> {
       ],
     );
   }
-
-  // Widget _buildMenu(String menuName) {
-  //   return Column(
-  //     crossAxisAlignment: CrossAxisAlignment.start,
-  //     children: [
-  //       Padding(
-  //         padding: const EdgeInsets.only(left: 22, top: 8, bottom: 10),
-  //         child: Text(
-  //           menuName,
-  //           style: GoogleFonts.poppins(
-  //             fontSize: 18,
-  //             fontWeight: FontWeight.w600,
-  //             color: Color.fromRGBO(126, 0, 0, 1),
-  //           ),
-  //         ),
-  //       ),
-  //       SingleChildScrollView(
-  //         scrollDirection: Axis.horizontal,
-  //         child: Row(
-  //           children: [
-  //             for (var menuItem in menuItems)
-  //               _buildMenuItem(menuName, menuItem),
-  //           ],
-  //         ),
-  //       ),
-  //     ],
-  //   );
-  // }
-
-  // Widget _buildMenuItem(String menuName, Map<String, dynamic> menuItem) {
-  //   final uniqueKey = '${menuName}-${menuItem['name']}';
-
-  //   return Container(
-  //     key: Key(uniqueKey),
-  //     margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
-  //     height: 120,
-  //     width: 300,
-  //     decoration: BoxDecoration(
-  //       color: Colors.white,
-  //       borderRadius: BorderRadius.circular(10),
-  //       boxShadow: [
-  //         BoxShadow(
-  //           color: Colors.grey.withOpacity(0.5),
-  //           spreadRadius: 1,
-  //           blurRadius: 6,
-  //         ),
-  //       ],
-  //     ),
-  //     child: Row(
-  //       children: [
-  //         Padding(
-  //           padding: EdgeInsets.all(15),
-  //           child: ClipRRect(
-  //             borderRadius: BorderRadius.circular(10),
-  //             child: Image.asset(
-  //               menuItem['imagePath'] ?? '',
-  //               height: 100,
-  //               width: 100,
-  //             ),
-  //           ),
-  //         ),
-  //         Column(
-  //           crossAxisAlignment: CrossAxisAlignment.start,
-  //           children: [
-  //             Padding(
-  //               padding: EdgeInsets.only(left: 10, right: 5, top: 30),
-  //               child: Text(
-  //                 menuItem['name'] ?? '',
-  //                 style: GoogleFonts.poppins(
-  //                   fontSize: 15,
-  //                   fontWeight: FontWeight.w600,
-  //                   color: Color.fromRGBO(202, 37, 37, 1),
-  //                 ),
-  //               ),
-  //             ),
-  //             Padding(
-  //               padding: EdgeInsets.only(left: 10, right: 5),
-  //               child: Text(
-  //                 "Rp${menuItem['price'] ?? 0}", // Display the price
-  //                 style: GoogleFonts.poppins(
-  //                   fontSize: 15,
-  //                   fontWeight: FontWeight.normal,
-  //                   color: Colors.black,
-  //                 ),
-  //               ),
-  //             ),
-  //             Row(
-  //               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-  //               children: [
-  //                 IconButton(
-  //                   icon: Icon(Icons.add_circle_outline,
-  //                       color: Color.fromRGBO(211, 36, 43, 1)),
-  //                   onPressed: () {},
-  //                 ),
-  //                 Text(
-  //                   (_menuQuantities[uniqueKey] ?? 0).toString(),
-  //                   style: GoogleFonts.poppins(
-  //                     fontSize: 16,
-  //                     fontWeight: FontWeight.bold,
-  //                   ),
-  //                 ),
-  //                 IconButton(
-  //                   icon: Icon(
-  //                     Icons.remove_circle_outline,
-  //                     color: Color.fromRGBO(211, 36, 43, 1),
-  //                   ),
-  //                   onPressed: () {},
-  //                 ),
-  //               ],
-  //             ),
-  //           ],
-  //         ),
-  //       ],
-  //     ),
-  //   );
-  // }
 }
 
 class TenantMenuCard extends StatefulWidget {
